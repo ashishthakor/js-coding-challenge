aa31b7cf5916300bf883fdda3528541d21a4c81c
ashishthakor1 <ashish.t@crestinfosystems.net>
2022-08-25 18:45:42 +0530

    Small Change in condition

--- question-11/compareObject.js
+++ question-11/compareObject.js
@@ -10,7 +10,8 @@ function compareObject(obj1, obj2) {
     const sortedObj1 = obj1.split("").sort().join("");
     const sortedObj2 = obj2.split("").sort().join("");
 
-    // console.log(sortedObj1, sortedObj2);
-    sortedObj1 !== sortedObj2 ? console.log("Both Objects are Simmiler") : console.log("Both Objects are not Simmiler");
+    // console.log("sortedObj1", sortedObj1, "sortedObj2", sortedObj2);
+    sortedObj1 === sortedObj2 ? console.log("Both Objects are Simmiler") : console.log("Both Objects are not Simmiler");
 }
-compareObject({ id: 1 }, { id: 2 });
+compareObject({ id: 1 }, { id: 1 });
+compareObject({ id: 1, name: "Ashish" }, { id: 2, name: "Ashish" });
